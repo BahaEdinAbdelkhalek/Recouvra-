17b2e117171c2dea715072e0d8b500bcae528f9e
baha <baha.edabdelkhalek@gmail.com>
2026-03-11 14:34:56 +0100

    baha : ajout de jwt authentification et le middleware

---
+++ utils/middleware/authMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.jwt;
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.userId).select('-password');
+
+    if (!req.user) {
+      res.status(401);
+      throw new Error('Not authorized, user not found');
+    }
+
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error('Not authorized, token invalid');
+  }
+});
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      res.status(401);
+      throw new Error('Not authorized');
+    }
+
+    if (!roles.includes(req.user.role)) {
+      res.status(403);
+      throw new Error(`Role '${req.user.role}' is not authorized to access this resource`);
+    }
+
+    next();
+  };
+};
+
+module.exports = { protect, authorize };
