26d2965114d262735431a1e37c0c9b7b1e17031d
baha <baha.edabdelkhalek@gmail.com>
2026-03-11 13:59:35 +0100

    Baha : structure de projet et setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Recouvra+ API' });
+});
+
+module.exports = app;
